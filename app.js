019139674dc45bdda8efaad301ac21aff68bafe5
Adam Bavosa <adam.bavosa@gmail.com>
2020-05-01 12:50:35 -0700

    init

---
+++ app.js
@@ -0,0 +1,234 @@
+const protocolData = window.protocolData;
+const erc20cTokenAbi = window.erc20cTokenAbi;
+const comptrollerAbi = window.comptrollerAbi;
+const networks = Object.keys(protocolData);
+const cTokenEndpoint = 'https://api.compound.finance/api/v2/ctoken';
+const infuraApiKey = '7db01e82204d4e789e22cf8e4f640ebe'
+let web3;
+
+const numbFormat = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 4,
+  maximumFractionDigits: 18
+}).format;
+
+Array.prototype.forEachWithCallback = function(callback, final) {
+  const arrayCopy = JSON.parse(JSON.stringify(this));
+  let index = -1;
+  const next = () => {
+    index++;
+    if (arrayCopy.length > 0) {
+      callback(arrayCopy.shift(), index, next);
+    } else {
+      if (final) final();
+    }
+  }
+  next();
+}
+
+const tableTemplate = Handlebars.compile(`
+  <div class="max-width center">
+    <h3>{{ name }}</h3>
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Attribute Name</th>
+            <th>Attribute Value</th>
+            <th>Member Name</th>
+            <th>Member Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Current Exchange Rate</td>
+            <td>{{ textExchangeRate }}</td>
+            <td>exchangeRateCurrent</td>
+            <td>{{ exchangeRateCurrent }}</td>
+          </tr>
+          <tr>
+            <td>Contract Holdings</td>
+            <td>{{ textContractHoldings }}</td>
+            <td>liquidityPoolTotal</td>
+            <td>{{ liquidityPoolTotal }}</td>
+          </tr>
+          <tr>
+            <td>Open Borrows Sum</td>
+            <td>{{ textOpenBorrows }}</td>
+            <td>totalBorrowsCurrent</td>
+            <td>{{ totalBorrowsCurrent }}</td>
+          </tr>
+          <tr>
+            <td>Supply Rate / Block</td>
+            <td>{{ textSupplyRate }}</td>
+            <td>supplyRatePerBlock</td>
+            <td>{{ supplyRatePerBlock }}</td>
+          </tr>
+          <tr>
+            <td>Borrow Rate / Block</td>
+            <td>{{ textBorrowRate }}</td>
+            <td>borrowRatePerBlock</td>
+            <td>{{ borrowRatePerBlock }}</td>
+          </tr>
+          <tr>
+            <td>cTokens in Circulation</td>
+            <td>{{ textCTokenCirculation }}</td>
+            <td>totalSupply, decimals</td>
+            <td>totalSupply / (1 * 10 ^ decimals)</td>
+          </tr>
+          <tr>
+            <td>Total Reserves</td>
+            <td>{{ textReservesSum }}</td>
+            <td>totalReserves</td>
+            <td>{{ totalReserves }}</td>
+          </tr>
+          <tr>
+            <td>Reserve Factor</td>
+            <td>{{ textReserveFactor }}</td>
+            <td>reserveFactorMantissa</td>
+            <td>{{ reserveFactor }}</td>
+          </tr>
+          <tr>
+            <td>Collateral Factor</td>
+            <td>{{ textCollateralFactor }}</td>
+            <td>comptroller.markets</td>
+            <td>{{ collateralFactor.collateralFactorMantissa }}</td>
+          </tr>
+          <tr>
+            <td>Underlying Address</td>
+            <td>{{ underlyingAddress }}</td>
+            <td>underlying</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+`);
+
+const networkTemplate = Handlebars.compile(`
+  <h2 class="capitalize">{{ this }}</h2>
+  <div id="{{ this }}">
+    <div class="loading-table">
+      <div class="loader"></div>
+    </div>
+    <div></div>
+  </div>
+`);
+
+const assetTemplate = Handlebars.compile(`
+  <div id="{{ this }}"></div>
+`);
+
+window.addEventListener('load', async () => {
+  const navigation = document.getElementById('navigation');
+  const networksContainer = document.getElementById('networks-container');
+  let networksHtml = '';
+  networks.forEach((net) => {
+    networksHtml += networkTemplate(net);
+  });
+  networksContainer.innerHTML = networksHtml;
+
+  networks.forEach((net) => {
+    const networkContainer = document.getElementById(net);
+    const loadingElement = networkContainer.children[0];
+    const tablesContainer = networkContainer.children[1];
+
+    let tablesHtml = '';
+    protocolData[net].cTokens.forEach((cToken, i) => {
+      const symbol = cToken.symbol;
+      tablesHtml += assetTemplate(`${net}-${symbol}`);
+    });
+
+    tablesContainer.innerHTML = tablesHtml;
+
+    protocolData[net].cTokens.forEachWithCallback(async (cToken, index, done) => {
+      const comptrollerAddr = protocolData[net].comptroller;
+      const cTokenAddr = cToken.token_address;
+      const symbol = cToken.symbol;
+      try {
+        const data = await getCTokenData(net, cTokenAddr, comptrollerAddr, symbol);
+        data.name = cToken.name;
+        loadingElement.classList.add('hidden');
+        tablesContainer.children[`${net}-${symbol}`].innerHTML += tableTemplate(data);
+
+        const nav = document.createElement("A");
+        nav.href = `#${net}-${symbol}`
+        nav.innerText = `${net} - ${symbol}`
+        navigation.appendChild(nav);
+        navigation.appendChild(document.createElement("BR"));
+      } catch (e) {
+        console.error(`${net} ${symbol}:`, e);
+      }
+      done();
+    });
+  });
+});
+
+const getCTokenData = (network, cTokenAddr, comptrollerAddr, symbol) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      web3 = new Web3(`https://${network}.infura.io/v3/${infuraApiKey}`);
+      const cToken = new web3.eth.Contract(erc20cTokenAbi, cTokenAddr);
+      const comptroller = new web3.eth.Contract(comptrollerAbi, comptrollerAddr);
+
+      const exchangeRateCurrent = await cToken.methods.exchangeRateCurrent().call();
+      const liquidityPoolTotal  = await cToken.methods.getCash().call();
+      const totalBorrowsCurrent = await cToken.methods.totalBorrowsCurrent().call();
+      const borrowRatePerBlock  = await cToken.methods.borrowRatePerBlock().call();
+      const totalSupply         = await cToken.methods.totalSupply().call();
+      const supplyRatePerBlock  = await cToken.methods.supplyRatePerBlock().call();
+      const totalReserves       = await cToken.methods.totalReserves().call();
+      const reserveFactor       = await cToken.methods.reserveFactorMantissa().call();
+      const collateralFactor    = await comptroller.methods.markets(cTokenAddr).call();
+      const cTokenDecimals      = await cToken.methods.decimals().call();
+      const cTokenMantissa      = parseFloat('1e'+cTokenDecimals);
+      const underlyingAddress   = await cToken.methods.underlying().call();
+
+      // const closeFactorMantissa = await comptroller.methods.closeFactorMantissa().call();
+      // const liquidationIncentiveMantissa = await comptroller.methods.liquidationIncentiveMantissa().call();
+
+      const result = {
+        exchangeRateCurrent,
+        liquidityPoolTotal,
+        totalBorrowsCurrent,
+        borrowRatePerBlock,
+        totalSupply,
+        supplyRatePerBlock,
+        totalReserves,
+        reserveFactor,
+        collateralFactor,
+        cTokenDecimals,
+        underlyingAddress,
+        textExchangeRate: `1 c${symbol} = ${numbFormat(exchangeRateCurrent / 1e18 / 1e10)} ${symbol}`,
+        textContractHoldings: `${numbFormat(liquidityPoolTotal / 1e18)} ${symbol}`,
+        textOpenBorrows: `${numbFormat(totalBorrowsCurrent / 1e18)} ${symbol}`,
+        textSupplyRate: `${(supplyRatePerBlock / 1e18).toFixed(18)} ${symbol} per ${symbol} supplied`,
+        textBorrowRate: `${(borrowRatePerBlock / 1e18).toFixed(18)} ${symbol} per ${symbol} borrowed`,
+        textCTokenCirculation: `${numbFormat(totalSupply / cTokenMantissa)} c${symbol}`,
+        textReservesSum: `${numbFormat(totalReserves / 1e18)} ${symbol}`,
+        textReserveFactor: `${reserveFactor / 1e18 * 100}%`,
+        textCollateralFactor: `${collateralFactor.collateralFactorMantissa / 1e18 * 100}%`,
+        textcTokenMantissa: cTokenMantissa
+      }
+
+      console.log(`
+        ~~ ${network} ~~ ${symbol} ~~
+        Current Exchange Rate: ${result.textExchangeRate}
+        ${symbol} in contract:       ${result.textContractHoldings}
+        Open Borrows Sum:      ${result.textOpenBorrows}
+        Supply Rate / Block:   ${result.textSupplyRate}
+        Borrow Rate / Block:   ${result.textBorrowRate}
+        c${symbol} in circulation:   ${result.textCTokenCirculation}
+        Reserves:              ${result.textReservesSum}
+        Reserve Factor:        ${result.textReserveFactor}
+        Collateral Factor:     ${result.textCollateralFactor}
+        cToken Mantissa:       ${cTokenMantissa}
+        Underlying Address:    ${underlyingAddress}
+      `);
+
+      resolve(result);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
---
+++ protocolData.js
@@ -0,0 +1,261 @@
+window.protocolData = {
+  "mainnet": {
+    "cTokens": [
+      {
+        "name": "Compound Augur",
+        "token_address": "0x158079ee67fce2f58472a96584a73c7ab9ac95c1",
+        "underlying_address": "0x1985365e9f78359a9b6ad760e32412f4a445e862",
+        "symbol": "REP"
+      },
+      {
+        "name": "Compound Sai",
+        "token_address": "0xf5dce57282a584d2746faf1593d3121fcac444dc",
+        "underlying_address": "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+        "symbol": "SAI"
+      },
+      {
+        "name": "Compound USD Coin",
+        "token_address": "0x39aa39c021dfbae8fac545936693ac917d5e7563",
+        "underlying_address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        "symbol": "USDC"
+      },
+      {
+        "name": "Compound Ether",
+        "token_address": "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5",
+        "underlying_address": null,
+        "symbol": "ETH"
+      },
+      {
+        "name": "Compound 0x",
+        "token_address": "0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407",
+        "underlying_address": "0xe41d2489571d322189246dafa5ebde1f4699f498",
+        "symbol": "ZRX"
+      },
+      {
+        "name": "Compound Basic Attention Token",
+        "token_address": "0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e",
+        "underlying_address": "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+        "symbol": "BAT"
+      },
+      {
+        "name": "Compound Dai",
+        "token_address": "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643",
+        "underlying_address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+        "symbol": "DAI"
+      },
+      {
+        "name": "Compound Wrapped BTC",
+        "token_address": "0xc11b1268c1a384e55c48c2391d8d480264a3a7f4",
+        "underlying_address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+        "symbol": "WBTC"
+      }
+    ],
+    "comptroller": "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b"
+  },
+  "ropsten": {
+    "cTokens": [
+      {
+        "name": "Compound SAI 📈",
+        "token_address": "0xccaf265e7492c0d9b7c2f0018bf6382ba7f0148d",
+        "underlying_address": "0x6f964c8c8c31b89d553ffdb30ef8f667cfbd0b9f",
+        "symbol": "SAI"
+      },
+      {
+        "name": "Compound Augur 📈",
+        "token_address": "0x5d4373f8c1af21c391ad7ec755762d8dd3cca809",
+        "underlying_address": "0x0a1e4d0b5c71b955c0a5993023fc48ba6e380496",
+        "symbol": "REP"
+      },
+      {
+        "name": "Compound Wrapped BTC 📈",
+        "token_address": "0x4d15ee7de1f86248c986f5ae7dce855b1c1a8806",
+        "underlying_address": "0xd83f707f003a1f0b1535028ab356fce2667ab855",
+        "symbol": "WBTC"
+      },
+      {
+        "name": "Compound Basic Attention Token 📈",
+        "token_address": "0xa253295ec2157b8b69c44b2cb35360016daa25b1",
+        "underlying_address": "0x9636246bf34e688c6652af544418b38eb51d2c43",
+        "symbol": "BAT"
+      },
+      {
+        "name": "Compound USDT",
+        "token_address": "0xb6f7f1901ffbcbadf9cd9831a032395105bc3142",
+        "underlying_address": "0x6ee856ae55b6e1a249f04cd3b947141bc146273c",
+        "symbol": "USDT"
+      },
+      {
+        "name": "Compound 0x 📈",
+        "token_address": "0x3a728dd027ad6f76cdea227d5cf5ba7ce9390a3d",
+        "underlying_address": "0x19787bcf63e228a6669d905e90af397dca313cfc",
+        "symbol": "ZRX"
+      },
+      {
+        "name": "Compound Trustless Bitcoin 📈",
+        "token_address": "0xb40d042a65dd413ae0fd85becf8d722e16bc46f1",
+        "underlying_address": "0x083f652051b9cdbf65735f98d83cc329725aa957",
+        "symbol": "TBTC"
+      },
+      {
+        "name": "Compound MultiCollateral DAI 📈",
+        "token_address": "0x6ce27497a64fffb5517aa4aee908b1e7eb63b9ff",
+        "underlying_address": "0xb5e5d0f8c0cba267cd3d7035d6adc8eba7df7cdd",
+        "symbol": "DAI"
+      },
+      {
+        "name": "Compound USD Coin 📈",
+        "token_address": "0x20572e4c090f15667cf7378e16fad2ea0e2f3eff",
+        "underlying_address": "0x8a9447df1fb47209d36204e6d56767a33bf20f9f",
+        "symbol": "USDC"
+      },
+      {
+        "name": "Compound Ether 📈",
+        "token_address": "0x1d70b01a2c3e3b2e56fcdcefe50d5c5d70109a5d",
+        "underlying_address": null,
+        "symbol": "ETH"
+      }
+    ],
+    "comptroller": "0xe03718b458a2e912141cf3fc8dab648362ee7463"
+  },
+  "rinkeby": {
+    "cTokens": [
+      {
+        "name": "Compound Basic Attention Token 📈",
+        "token_address": "0xebf1a11532b93a529b5bc942b4baa98647913002",
+        "underlying_address": "0xbf7a7169562078c96f0ec1a8afd6ae50f12e5a99",
+        "symbol": "BAT"
+      },
+      {
+        "name": "Compound 0x 📈",
+        "token_address": "0x52201ff1720134bbbbb2f6bc97bf3715490ec19b",
+        "underlying_address": "0xddea378a6ddc8afec82c36e9b0078826bf9e68b6",
+        "symbol": "ZRX"
+      },
+      {
+        "name": "Compound Dai 📈",
+        "token_address": "0x6d7f0754ffeb405d23c51ce938289d4835be3b14",
+        "underlying_address": "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea",
+        "symbol": "DAI"
+      },
+      {
+        "name": "Compound Ether 📈",
+        "token_address": "0xd6801a1dffcd0a410336ef88def4320d6df1883e",
+        "underlying_address": null,
+        "symbol": "ETH"
+      },
+      {
+        "name": "Compound Wrapped BTC 📈",
+        "token_address": "0x0014f450b8ae7708593f4a46f8fa6e5d50620f96",
+        "underlying_address": "0x577d296678535e4903d59a4c929b718e1d575e0a",
+        "symbol": "WBTC"
+      },
+      {
+        "name": "Compound Augur 📈",
+        "token_address": "0xebe09eb3411d18f4ff8d859e096c533cac5c6b60",
+        "underlying_address": "0x6e894660985207feb7cf89faf048998c71e8ee89",
+        "symbol": "REP"
+      },
+      {
+        "name": "Compound USD Coin 📈",
+        "token_address": "0x5b281a6dda0b271e91ae35de655ad301c976edb1",
+        "underlying_address": "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b",
+        "symbol": "USDC"
+      }
+    ],
+    "comptroller": "0x2eaa9d77ae4d8f9cdd9faacd44016e746485bddb"
+  },
+  "kovan": {
+    "cTokens": [
+      {
+        "name": "Compound Augur",
+        "token_address": "0xfd874be7e6733bdc6dca9c7cdd97c225ec235d39",
+        "underlying_address": "0x4E5cB5A0CAca30d1ad27D8CD8200a907854FB518",
+        "symbol": "REP"
+      },
+      {
+        "name": "Compound Sai",
+        "token_address": "0x63c344bf8651222346dd870be254d4347c9359f7",
+        "underlying_address": "0xC4375B7De8af5a38a93548eb8453a498222C4fF2",
+        "symbol": "SAI"
+      },
+      {
+        "name": "Compound USD Coin",
+        "token_address": "0xcfc9bb230f00bffdb560fce2428b4e05f3442e35",
+        "underlying_address": "0x75B0622Cec14130172EaE9Cf166B92E5C112FaFF",
+        "symbol": "USDC"
+      },
+      {
+        "name": "Compound 0x",
+        "token_address": "0xc014dc10a57ac78350c5fddb26bb66f1cb0960a0",
+        "underlying_address": "0x29eb28bAF3B296b9F14e5e858C52269b57b4dF6E",
+        "symbol": "ZRX"
+      },
+      {
+        "name": "Compound Basic Attention Token",
+        "token_address": "0xd5ff020f970462816fdd31a603cb7d120e48376e",
+        "underlying_address": "0x9dDB308C14f700d397bB26F584Ac2E303cdc7365",
+        "symbol": "BAT"
+      },
+      {
+        "name": "Compound Dai",
+        "token_address": "0xe7bc397dbd069fc7d0109c0636d06888bb50668c",
+        "underlying_address": "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa",
+        "symbol": "DAI"
+      },
+      {
+        "name": "Compound Wrapped BTC",
+        "token_address": "0x3659728876efb2780f498ce829c5b076e496e0e3",
+        "underlying_address": "0xA0A5aD2296b38Bd3e3Eb59AAEAF1589E8d9a29A9",
+        "symbol": "WBTC"
+      }
+    ],
+    "comptroller": "0x1f5d7f3caac149fe41b8bd62a3673fe6ec0ab73b"
+  },
+  "goerli": {
+    "cTokens": [
+      {
+        "name": "Compound Augur",
+        "token_address": "0x1d70b01a2c3e3b2e56fcdcefe50d5c5d70109a5d",
+        "underlying_address": "0x183Faf58c4461972765f3F90c6272A4ecE66Bd96",
+        "symbol": "REP"
+      },
+      {
+        "name": "Compound Sai",
+        "token_address": "0x5d4373f8c1af21c391ad7ec755762d8dd3cca809",
+        "underlying_address": "0x8e9192D6f9d903b1BEb3836F52a9f71E05846e42",
+        "symbol": "SAI"
+      },
+      {
+        "name": "Compound USD Coin",
+        "token_address": "0xcec4a43ebb02f9b80916f1c718338169d6d5c1f0",
+        "underlying_address": "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C",
+        "symbol": "USDC"
+      },
+      {
+        "name": "Compound 0x",
+        "token_address": "0xa253295ec2157b8b69c44b2cb35360016daa25b1",
+        "underlying_address": "0xe4E81Fa6B16327D4B78CFEB83AAdE04bA7075165",
+        "symbol": "ZRX"
+      },
+      {
+        "name": "Compound Basic Attention Token",
+        "token_address": "0xccaf265e7492c0d9b7c2f0018bf6382ba7f0148d",
+        "underlying_address": "0x70cBa46d2e933030E2f274AE58c951C800548AeF",
+        "symbol": "BAT"
+      },
+      {
+        "name": "Compound Dai",
+        "token_address": "0x822397d9a55d0fefd20f5c4bcab33c5f65bd28eb",
+        "underlying_address": "0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60",
+        "symbol": "DAI"
+      },
+      {
+        "name": "Compound Wrapped BTC",
+        "token_address": "0xa253295ec2157b8b69c44b2cb35360016daa25b1",
+        "underlying_address": "0xe4E81Fa6B16327D4B78CFEB83AAdE04bA7075165",
+        "symbol": "WBTC"
+      }
+    ],
+    "comptroller": "0x627ea49279fd0de89186a58b8758ad02b6be2867"
+  }
+}
